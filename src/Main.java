658ea834d26d0560f853d75583fa111949d3087c
korchemkin <dmitri.korchemkin@gmail.com>
2019-11-16 16:05:37 +0300

    Add merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 import com.github.korchemkin.Sorter;
+
 import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
-        int[] unsortedArr = {9, 5, 8, 1, 3, 7, 2, 6, 4, 0};
+        int[] unsortedArr = { 9, 5, 8, 1, 3, 7, 2, 6, 4, 0 };
 
         Sorter sorter = new Sorter();
         int[] insertionSortResult = sorter.insertionSort(unsortedArr);
         int[] selectionSortResult = sorter.selectionSort(unsortedArr);
         int[] bubbleSortResult = sorter.bubbleSort(unsortedArr);
+        int[] mergeSortResult = sorter.mergeSort(unsortedArr);
 
         System.out.println("unsortedArr " + Arrays.toString(unsortedArr));
         System.out.println("insertionSortResult " + Arrays.toString(insertionSortResult));
         System.out.println("selectionSortResult " + Arrays.toString(selectionSortResult));
         System.out.println("bubbleSortResult " + Arrays.toString(bubbleSortResult));
+        System.out.println("mergeSortResult " + Arrays.toString(mergeSortResult));
     }
 }
---
+++ src/com/github/korchemkin/MergeSorter.java
@@ -0,0 +1,4 @@
+package com.github.korchemkin;
+
+public class MergeSorte {
+}
